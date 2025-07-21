c822bd208df0b726d73b856239e3666d1b286a1b
RajneeshYadav-123 <rajneeshyadav0507@gmail.com>
2025-07-22 01:51:33 +0530

    cors fix

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,10 @@ const express=require('express');
 const app=express();
 require("dotenv").config();
 const cors=require('cors');
-app.use(cors());
+app.use(cors({
+  origin: *,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
